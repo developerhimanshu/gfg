6cc4b952a88387790cb236eeeec782de187069b3
HIMANSHU SINGH <71926216+developerhimanshu@users.noreply.github.com>
2022-06-12 23:07:33 +0530

    Add files via upload

---
+++ 18Greedy Algorithm/3fractionalKnapsack.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool mycmp(pair<int, int>a, pair<int, int>b) {   //This comparator is for sorting the pairs in the value/weight ratio
+	double r1 = (double)a.second / a.first;
+	double r2 = (double)b.second / b.first;
+	return r1 > r2;
+}
+int fKnapS(vector<pair<int, int>>things, int weight) {
+	sort(things.begin(), things.end(), mycmp);
+	int curr_weight = weight;
+	double res = 0.0;
+	for (int i = 0; i < things.size(); i++) {
+		if (things[i].first <= weight) {
+			weight -= things[i].first;
+			res += things[i].second;
+		}
+		else {
+			res += ((double)weight / things[i].first) * (things[i].second);
+			break;
+		}
+	}
+	return res;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<pair<int, int>>arr;
+    while(n--){
+        int weight, value;
+        cin>>weight>>value;
+        arr.push_back(make_pair(weight,value));
+    }
+
+	int W = 50;
+
+	cout << "The maximum value can be optained from fractional KnapSack is: "<<fKnapS(arr, W);
+
+
+	return 0;
+}
